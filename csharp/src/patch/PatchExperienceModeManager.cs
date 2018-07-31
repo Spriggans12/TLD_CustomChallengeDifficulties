e46060e960218e512aae9130f08bf88c2d9c8557
Spriggans <vincent.justin12@gmail.com>
2018-07-31 08:51:55 +0200

    Patch ExpModeManager

---
+++ csharp/src/patch/PatchExperienceModeManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using UnityEngine;
+
+namespace CustomChallengeDifficulties {
+
+    [HarmonyPatch(typeof(ExperienceModeManager), "InCustomMode")]
+    class PatchExperienceModeManagerInCustomMode {
+
+        // - returns a boolean that controls if original is executed (true) or not (false)
+        static bool Prefix(ExperienceModeManager __instance, bool __result) {
+            ExperienceModeType _m = __instance.GetCurrentExperienceModeType();
+            if(_m == ExperienceModeType.ChallengeHunted
+                || _m == ExperienceModeType.ChallengeHuntedPart2
+                || _m == ExperienceModeType.ChallengeNomad
+                || _m == ExperienceModeType.ChallengeRescue
+                || _m == ExperienceModeType.ChallengeWhiteout) {
+                __result = true;
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(ExperienceModeManager), "GetCustomMode")]
+    class PatchExperienceModeManagerGetCustomMode {
+
+        // - returns a boolean that controls if original is executed (true) or not (false)
+        static bool Prefix(ExperienceModeManager __instance, __) {
+
+
+            return true;
+        }
+    }
+}
